c702c56fc67e136041ffd188c4744dc31fc509b8
Andrii Soroka <dree0103@gmail.com>
2017-07-26 12:55:41 +0300

    Update getTopNRichestNames.js

--- js/getTopNRichestNames.js
+++ js/getTopNRichestNames.js
@@ -1,10 +1,18 @@
 function getTopNRichestNames (numb, arr) {
-	var newArr1 = [];
-	if(numb > arr.length) numb = arr.length;
-		for ( i = 0; i < numb; i++) {
-				newArr1.push(arr[i].name);
-	};
-	return newArr1;
+	function culc (income) {
+		var B = 1e6, M = 1e6, K = 1e3;
+		return parseInt(income.charAt(0)) * eval(income.charAt(1));
+	}
+	var newArr = arr.sort(function(obj1, obj2){
+		if ( culc(obj1.income) < culc(obj2.income) ) {
+			return 1;
+		}
+		if ( culc(obj1.income) > culc(obj2.income) ) {
+			return -1;
+		}
+		return 0;
+	});
+	return [pluckByAttribute(newArr.slice(0, numb), 'name'), newArr.slice(0, numb)];
 }
 var people = [
     {name: 'Bara', income: '1B'},
